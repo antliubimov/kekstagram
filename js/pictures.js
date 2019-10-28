0d48467041d8eee1ba4e70dacd3858a09053d8f3
Anton Liubimov <ant.liubimov@gmail.com>
2019-10-28 13:01:52 +0500

    rename variables

--- js/pictures.js
+++ js/pictures.js
@@ -1,45 +1,63 @@
 'use strict';
+var PHOTO_COUNT = 25;
+var LIKES_MIN = 15;
+var LIKES_MAX = 200;
+var COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппаратна кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var DESCRIPTIONS = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
 
-var randomNum = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
+var getRandomNum = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
+
+var getRandomElement = elements => elements[getRandomNum(0, elements.length - 1)];
 
 var randomArr = (min, max) => {
   var arr = [];
+
   while (arr.length !== max) {
-    var arrEl = randomNum(min, max);
+    var arrEl = getRandomNum(min, max);
+
     if (!arr.includes(arrEl)) {
       arr.push(arrEl);
     }
   }
+
   return arr;
 };
 
 var urlRandNum = randomArr(1, 25);
 
-var comments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппаратна кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var getComments = () => {
+  var countComments = getRandomNum(1, 2);
+  var comments = [];
 
-var commentsArr = () => {
-  var length = randomNum(1, 2);
-  var arr = [];
-  while (arr.length !== length) {
-    var arrEl = randomNum(0, comments.length - 1);
-    if (!arr.includes(arrEl)) {
-      arr.push(arrEl);
+  while (comments.length !== countComments) {
+    var comment = getRandomElement(COMMENTS);
+
+    if (!comments.includes(comment)) {
+      comments.push(comment);
     }
   }
-  var commArr = arr.map(el => comments[el]);
-  return commArr;
-};
 
-var description = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
+  return comments;
+};
 
 var photos = [];
 
 for (var i = 0; i < 25; i++) {
   var photo = {
     url: `photos/${urlRandNum[i]}.jpg`,
-    likes: randomNum(15, 200),
-    comments: commentsArr(),
-    description: description[randomNum(0, description.length - 1)]
+    likes: getRandomNum(LIKES_MIN, LIKES_MAX),
+    comments: getComments(),
+    description: getRandomElement(DESCRIPTIONS)
   };
+
   photos.push(photo);
 }
+
+var photoTemplate = document.querySelector('#picture-template');
+
+var renderPhoto = (photo) => {
+  var photoElement = photoTemplate.cloneNode(true);
+  photoElement.querySelector('img').src = photo.url;
+  photoElement.querySelector('.picture__likes').textContent = photo.likes;
+  photoElement.querySelector('.picture__comments')
+};
