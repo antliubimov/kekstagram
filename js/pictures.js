c1c3647633f23ac5ac203e7b58979bd0ba3c5f99
Anton Liubimov <ant.liubimov@gmail.com>
2019-11-08 09:13:28 +0500

    done refactor code of the validation hashtags and comments

--- js/pictures.js
+++ js/pictures.js
@@ -304,7 +304,7 @@ var setPictureEffect = (effectName) => {
 /**
  * Add eventlisteners on effectItems and set the class and the effect on the upload image
  */
-var initialize = () => {
+var initializeEffects = () => {
   [...effectItems].forEach(effectsItem => effectsItem.addEventListener('click', onEffectsItemClick));
   defaultEffect.checked = true;
   setPictureClass(defaultEffect.value);
@@ -314,7 +314,7 @@ var initialize = () => {
 /**
  * remove Eventlisteners on effectItems when the upload form is closed
  */
-var finalize = () => {
+var finalizeEffects = () => {
   [...effectItems].forEach(effectsItem => effectsItem.removeEventListener('click', onEffectsItemClick));
 };
 /**
@@ -325,7 +325,7 @@ var imgUploadClose = () => {
   imgUploadPanel.classList.add('hidden');
   uploadCancel.removeEventListener('click', onUploadCancelClick);
   document.removeEventListener('down', onImgUploadEscDown);
-  finalize();
+  finalizeEffects();
 };
 /**
  * Click on Esc-button when the upload form is opened
@@ -390,7 +390,7 @@ var show = () => {
  */
 var onUploadFileChange = () => {
   imgUploadPanel.classList.remove('hidden');
-  initialize();
+  initializeEffects();
   uploadCancel.addEventListener('click', onUploadCancelClick);
   document.addEventListener('keydown', onImgUploadEscDown);
 };
@@ -409,13 +409,30 @@ var onScalePinMouseDown = () => {
 * */
 var hashTag = {
   SYMBOL: '#',
-  MIN_SIZE: 2,
+  MIN_SIZE: 1,
   MAX_SIZE: 20,
   AMOUNT: 5
 };
 
 const COMMENT_MAX_SIZE = 140;
 const ERROR_STYLE = `2px solid #ff0000`;
+
+
+/**
+ * Selects an incorrectly filled field
+ * @param {Node} element
+ */
+const setErrorValidStyle = (element) => {
+  element.style.border = ERROR_STYLE;
+};
+
+/**
+ * Deselects an incorrectly filled field
+ * @param {Node} element
+ */
+const resetErrorValidStyle = (element) => {
+  element.style.border = ``;
+};
 /**
  * Returns the array without the deleted element
  * @param {Array} initialArray
@@ -434,32 +451,63 @@ var getHashTagsArray = (str) => {
   return getArrayWithoutElement(hashTagsArray, '');
 };
 
-var onHashTagsInput = (evt) => {
-  var hashTagsArr = getHashTagsArray(evt.target.value);
-  var hashTagsArrLength = hashTagsArr.length;
-  var filterArr = hashTagsArr.filter(hashtag => hashtag[0] === '#');
-  var filterLengthArr = hashTagsArr.filter(hashtag => hashtag.length > hashTag.MAX_SIZE);
-  var filterSymbolArr = hashTagsArr.filter(hashtag => hashtag.match(/[^#a-zA-Z0-9а-яА-Я]/g));
-  var noRepeatHashTags = new Set(hashTagsArr);
-  if (hashTagsArrLength !== filterArr.length) {
-    hashTagsField.setCustomValidity('Каждый хэш-тег должен начинаться со знака #');
-  } else if (filterSymbolArr.length !== 0) {
-    hashTagsField.setCustomValidity('Каждый хеш-тег должен содержать знак решетки, затем буквы английского или русского алфавита и цифры');
-  } else if (hashTagsArr.includes('#')) {
-    hashTagsField.setCustomValidity('Хеш-тег не может состоять только из одной решётки');
-  } else if (hashTagsArrLength !== noRepeatHashTags.size) {
-    hashTagsField.setCustomValidity('Один и тот же хэш-тег не может быть использован дважды');
-  } else if (hashTagsArrLength > hashTag.AMOUNT) {
-    hashTagsField.setCustomValidity('Нельзя указать больше пяти хэш-тегов');
-  } else if (filterLengthArr.length > 0) {
-    hashTagsField.setCustomValidity('Максимальная длина одного хэш-тега 20 символов, включая решётку');
+/**
+ * Array of objects matching the validation function hashtag format and error message
+ * @type {*[]}
+ */
+var checkActions = [
+  {
+    message: 'Каждый хэш-тег должен начинаться со знака #',
+    check: (arg) => arg.some(elem => elem[0] !== hashTag.SYMBOL)
+  },
+  {
+    message: 'Каждый хеш-тег должен содержать знак решетки, затем буквы английского или русского алфавита и цифры',
+    check: (arg) => arg.some(elem => elem.match(/[^#a-zA-Z0-9а-яА-Я]/g))
+  },
+  {
+    message: 'Хеш-тег не может состоять только из одной решётки',
+    check: (arg) => arg.some(elem => elem.length < hashTag.MIN_SIZE)
+  },
+  {
+    message: 'Хэш-тэги должны быть уникальными',
+    check: (arg) => arg.some((value, idx, arr) => arr.indexOf(value) !== idx)
+  },
+  {
+    message: `Нельзя указать больше ${hashTag.AMOUNT} хэш-тегов`,
+    check: (arg) => arg.length > hashTag.AMOUNT
+  },
+  {
+    message: `Максимальная длина одного хэш-тега ${hashTag.MAX_SIZE} символов, включая решётку`,
+    check: (arg) => arg.some(elem => elem.length > hashTag.MAX_SIZE)
+  },
+  {
+    message: false,
+    check: (arg) => arg.length === 0
+  },
+  {
+    message: false,
+    check: (arg) => arg
+  }
+];
+/**
+ * Returns the first object of the checkActions array, whose check function from argument arg returns true
+ * @param {Array} arg
+ * @returns {Object}
+ */
+const getCheckAction = (arg) => checkActions.find(({check}) => check(arg));
+/**
+ * Checks the correctness of filling the field with hashtags. Шf the field is filled incorrectly displays an error message
+ * @param {String} data
+ */
+const checkHashTags = (data) => {
+  const hashTags = getHashTagsArray(data);
+  const {message} = getCheckAction(hashTags);
+  if (message) {
+    hashTagsField.setCustomValidity(message);
   } else {
     hashTagsField.setCustomValidity('');
   }
 };
-hashTagsField.addEventListener('input', onHashTagsInput);
-
-
 
 /**
  * Check max length of data-comments, if it is wrong shows error-message.
@@ -467,33 +515,42 @@ hashTagsField.addEventListener('input', onHashTagsInput);
  */
 var checkComment = (data) => {
   if (data.length > COMMENT_MAX_SIZE) {
-    data.setCustomValidity(`Длина комментария должно быть не более ${COMMENT_MAX_SIZE} символов. Текущая длина комментария ${data.length} символов`);
+    commentField.setCustomValidity(`Длина комментария должно быть не более ${COMMENT_MAX_SIZE} символов. Текущая длина комментария ${data.length} символов`);
   } else {
-    data.setCustomValidity('');
+    commentField.setCustomValidity('');
   }
-}
-
-var onTextDescriptionInput = (evt) => {
- checkComment(evt.target);
-};
-commentField.addEventListener('input', onTextDescriptionInput);
-
-var CustomValidation = (input) => {
-
 };
 
-var hashTagsFieldValidityChecks = [
-
-];
-
-hashTagsField.CustomValidation = new CustomValidation(hashTagsField);
-hashTagsField.CustomValidation.validityChecks = hashTagsFieldValidityChecks;
-
-var validate = () => {
-  hashTagsField.CustomValidation.checkInput();
+/**
+ * Hides messages about incorrectly filled fields at input event
+ * @param {Nodes} fields
+ */
+const clearCustomValidity = (...fields) => {
+  fields.forEach((field) => {
+    field.addEventListener('input', () => {
+      field.setCustomValidity('');
+    });
+  });
 };
 
+/**
+ * Initialize validation of fields
+ */
+const initializeValidity = () => {
+  clearCustomValidity(hashTagsField, commentField);
 
-submitButton.addEventListener('click', validate);
-uploadForm.addEventListener('submit', validate);
+  submitButton.addEventListener('click', () => {
+    checkHashTags(hashTagsField.value);
+    checkComment(commentField.value);
+  });
 
+  uploadForm.addEventListener('invalid', function (evt) {
+    setErrorValidStyle(evt.target);
+  }, true);
+
+  uploadForm.addEventListener('input', function (evt) {
+    resetErrorValidStyle(evt.target);
+  });
+};
+
+initializeValidity();
