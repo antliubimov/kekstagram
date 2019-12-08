7ca3571d826e5e08296e9586ef0c5c6198469a88
Anton Liubimov <ant.liubimov@gmail.com>
2019-12-08 17:06:30 +0500

    rechanged var on const

--- js/utils.js
+++ js/utils.js
@@ -1,21 +1,21 @@
 // utils.js
-'use strict';
+"use strict";
 
-(function () {
+(function() {
   /**
    * Returns a random number from min-value to max-value (inclusive)
    * @param {number} min
    * @param {number} max
    * @returns {number}
    */
-  var getRandomNum = (min, max) =>
+  const getRandomNum = (min, max) =>
     Math.round(min - 0.5 + Math.random() * (max - min + 1));
   /**
    * Returns an random element from an array
    * @param {array} elements
    * @returns {element} element
    */
-  var getRandomElement = elements =>
+  const getRandomElement = elements =>
     elements[getRandomNum(0, elements.length - 1)];
   /**
    * Returns array with numbers from min-value to max-value
@@ -23,11 +23,11 @@
    * @param {number} max
    * @returns {[]} array
    */
-  var getRandomArr = (min, max) => {
-    var arr = [];
+  const getRandomArr = (min, max) => {
+    const arr = [];
 
     while (arr.length !== max) {
-      var arrEl = getRandomNum(min, max);
+      const arrEl = getRandomNum(min, max);
 
       if (!arr.includes(arrEl)) {
         arr.push(arrEl);
@@ -37,9 +37,9 @@
     return arr;
   };
 
-  return window.utils = {
+  window.utils = {
     getRandomNum,
     getRandomElement,
-    getRandomArr,
-  }
+    getRandomArr
+  };
 })();
