a1019be7771662abddf620269c6e50b3c1504dd5
Anton Liubimov <ant.liubimov@gmail.com>
2019-12-08 17:00:25 +0500

    done js/picture.js

---
+++ js/picture.js
@@ -0,0 +1,32 @@
+// picture.js
+"use strict";
+
+(function() {
+  const photoTemplate = document
+    .querySelector("#picture")
+    .content.querySelector(".picture__link");
+
+  /**
+   * Render photo-element
+   * @param {object} photo
+   * @returns {Node}
+   */
+  const renderPhoto = photo => {
+    const photoElement = photoTemplate.cloneNode(true);
+    photoElement.querySelector(".picture__img").src = photo.url;
+    photoElement.querySelector(".picture__stat--likes").textContent =
+      photo.likes;
+    photoElement.querySelector(".picture__stat--comments");
+
+    photoElement.addEventListener(
+      "click",
+      window.gallery.onPhotoElementClick.bind(null, photo)
+    );
+
+    return photoElement;
+  };
+
+  window.picture = {
+    renderPhoto
+  };
+})();
