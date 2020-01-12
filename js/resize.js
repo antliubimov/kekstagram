ffffd8b288fa793d88dfff185a8efe3cf263d298
Anton Liubimov <ant.liubimov@gmail.com>
2020-01-12 23:35:30 +0500

    done decrease- and increase- sizeButtonClick

---
+++ js/resize.js
@@ -0,0 +1,46 @@
+// resize.js
+"use strict";
+
+(function() {
+  const PERCENT = {
+    min: 25,
+    max: 100,
+    step: 25
+  };
+  const decreaseSizeButton = document.querySelector(".resize__control--minus");
+  const increaseSizeButton = document.querySelector(".resize__control--plus");
+  const resizeValue = document.querySelector(".resize__control--value");
+  const img = document.querySelector(".img-upload__preview");
+
+  resizeValue.value = `${PERCENT.max}%`;
+
+  const getResizeValue = () => {
+    const regExp = /\d+[^%]/;
+    const resize = resizeValue.value;
+    return Number(resize.match(regExp)[0]);
+  };
+
+  const setResizeValue = resize => {
+    resizeValue.value = `${resize}%`;
+    const percentTransform = resize / 100;
+    img.style.transform = `scale(${percentTransform})`;
+  };
+
+  const onDecreaseSizeButtonClick = () => {
+    let resize = getResizeValue();
+    if (resize > PERCENT.min && resize <= PERCENT.max) {
+      resize -= PERCENT.step;
+    }
+    setResizeValue(resize);
+  };
+  const onIncreaseSizeButtonClick = () => {
+    let resize = getResizeValue();
+    if (resize >= PERCENT.min && resize < PERCENT.max) {
+      resize += PERCENT.step;
+    }
+    setResizeValue(resize);
+  };
+
+  decreaseSizeButton.addEventListener("click", onDecreaseSizeButtonClick);
+  increaseSizeButton.addEventListener("click", onIncreaseSizeButtonClick);
+})();
